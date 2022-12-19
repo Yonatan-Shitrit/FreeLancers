26c1af2d38aabb249cabdbaea6d4a8bf30f260df
Yonatan Shitrit <77463072+Yonatan-Shitrit@users.noreply.github.com>
2022-12-19 17:33:53 +0200

    Delete sprint-4-backend directory

--- sprint-4-backend/config/prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // 'dbURL': 'mongodb://localhost:27017',
-  'dbURL': `mongodb+srv://boby69:${process.env.atlas}@cluster0.camgr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-}
